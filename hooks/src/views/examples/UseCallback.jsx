b11f1d7ab6f2dd20d6d873c253baecb2eb6912d3
Alex Sousa <contato.fcoalex@gmail.com>
2021-05-07 13:05:51 +0000

    adiciona hooks até useContext

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButttons from './UseCallbackButtons'
 
 const UseCallback = (props) => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback((delta) => {
+        setCount(curr => curr + delta)
+    }, [setCount])
+
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+
+
+            <div className="center">
+                <span className="text">
+                    {count}
+                </span>
+
+                <UseCallbackButttons inc={inc} />
+            </div>
         </div>
     )
 }
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,64 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import DataContext from '../../data/DataContext'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
+
+    const {state, setState} = useContext(DataContext)
+
+    function addNumber(delta) {
+        setState({
+            ...state,
+            number: state.number + delta
+        })
+    }
+
+    //outra forma (ideal)
+
+    const {number, setNumber, text } = useContext(AppContext)
+
+    // =======
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <SectionTitle title="Exercicio 01" />
+            <div className="center">
+                <span className="text">
+                    {state.text}
+                </span>
+
+                <span className="text">
+                    {state.number}
+                </span>
+
+                <div>
+                    <button className="btn" onClick={() => addNumber(1)}>+1</button>
+                    <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercicio 02" />
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+
+                <div>
+                    <button className="btn" onClick={() => setNumber(number-1)}>
+                        -1
+                    </button>
+
+                    <button className="btn" onClick={() => setNumber(number+1)}>
+                        +1
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
