e9f74e2053265b5ee4e60ed4b24255956ea97544
Alex Sousa <contato.fcoalex@gmail.com>
2021-06-04 18:52:31 +0000

    finaliza express

--- node/express/index.js
+++ node/express/index.js
@@ -5,8 +5,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.get('/blog', (req, res) => {
-  res.send("Welcome to my Blog!")
+app.get("/ola/:nome",  (req, res) => { // com params
+  const { nome } = req.params
+  res.send(`<h1>Hi ${nome}!</h1>`)
+})
+
+app.get('/blog/:artigo?', (req, res) => { // com params opcional
+  const { artigo } = req.params
+
+  if(artigo) res.send(`<h2>Artigo: ${artigo}</h2>`)
+  else res.send("Welcome to my Blog!")
+  
+})
+
+app.get("/canal/youtube", (req, res) => {
+  const canal = req.query["canal"]
+  res.send(canal ?? '')
 })
 
 app.listen(3000, (error) => {
