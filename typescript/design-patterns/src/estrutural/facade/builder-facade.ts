6a90a74afb0675bba58ae7bfdda4921d8752e4bc
Alex Sousa <contato.fcoalex@gmail.com>
2021-09-09 13:23:35 +0000

    add facade typescript

---
+++ typescript/design-patterns/src/estrutural/facade/builder-facade.ts
@@ -0,0 +1,24 @@
+import { MainDishBuilder } from '../../creational/builder/classes/main-dish-builder';
+import { VeganDishBuilder } from '../../creational/builder/classes/vegan-dish-builder';
+
+export class BuilderFacade {
+  /* Essa é a fachada para a pasta src/creational/builder/index.ts */
+  private mainDishBuilder = new MainDishBuilder();
+  private veganDishBuilder = new VeganDishBuilder();
+
+  makeMeal1(): void {
+    this.mainDishBuilder.makeMeal().makeDessert();
+    console.log(this.mainDishBuilder.getPrice());
+  }
+
+  makeMeal2(): void {
+    this.mainDishBuilder.reset();
+    const meal2 = this.mainDishBuilder.makeBeverage().getMeal();
+    console.log(meal2);
+  }
+
+  makeMeal3(): void {
+    const meal3 = this.veganDishBuilder.makeMeal().getMeal();
+    console.log(meal3);
+  }
+}
